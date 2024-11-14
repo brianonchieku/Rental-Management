539b1839160f0c32e1c337d6ea3da7013145ab58
brianonchieku <brianonchikeku0@gmail.com>
2024-11-15 00:33:51 +0300

    users

--- app/src/main/java/com/example/rentals/model/UserRepository.kt
+++ app/src/main/java/com/example/rentals/model/UserRepository.kt
@@ -1,5 +1,8 @@
 package com.example.rentals.model
 
+import android.content.Context
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import kotlinx.coroutines.tasks.await
@@ -24,5 +27,30 @@ class UserRepository {
 
         }
         return users
+    }
+
+    fun addUser(user: User, context: Context){
+        val db = FirebaseFirestore.getInstance()
+        val auth = FirebaseAuth.getInstance()
+
+        auth.createUserWithEmailAndPassword(user.email, user.password).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                val current = auth.currentUser
+                if (current != null){
+                    val userId = current.uid
+                    val details = hashMapOf(
+                        "Name" to user.name,
+                        "Email" to user.email,
+                        "Phone Number" to user.number,
+                        "Role" to user.role
+                    )
+
+                    db.collection("Users").document(userId).set(details).addOnSuccessListener {
+                        Toast.makeText(context, "Added successfully", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+        }
     }
 }
