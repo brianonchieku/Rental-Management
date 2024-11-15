0a6e8fa215165bca1bab153e08469ac19ca4ca9e
brianonchieku <brianonchikeku0@gmail.com>
2024-11-15 15:23:53 +0300

    users

---
+++ app/src/main/java/com/example/rentals/view/UsersPge.kt
@@ -0,0 +1,67 @@
+package com.example.rentals.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UsersPage(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+            .padding(top = 30.dp),
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        Scaffold (topBar = {
+            TopAppBar(title = { Text(text = "Users") }, navigationIcon = {
+                Icon(imageVector = Icons.Default.Person, contentDescription =null )
+            })
+        }){ paddingValues ->
+            Box (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                contentAlignment = Alignment.Center
+            ){
+                Column(
+                    modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row (
+                        modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End
+                    ){
+                        Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(15.dp)) {
+                            Text(text = "Add user")
+                            
+                        }
+                        
+                    }
+
+                }
+
+            }
+
+
+        }
+
+    }
+}
