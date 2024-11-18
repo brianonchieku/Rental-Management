598e67d8a2359586995444cfe17331c05b83513d
brianonchieku <brianonchikeku0@gmail.com>
2024-11-18 15:42:32 +0300

    users

--- app/src/main/java/com/example/rentals/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/rentals/viewmodel/UserViewModel.kt
@@ -5,20 +5,31 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.rentals.NetworkResponse
 import com.example.rentals.model.User
 import com.example.rentals.model.UserRepository
+import com.google.firebase.firestore.FirebaseFirestoreException
 import kotlinx.coroutines.launch
 
 class UserViewModel: ViewModel() {
 
     private val userRepository: UserRepository = UserRepository()
-    private val _users = MutableLiveData<List<User>>(emptyList())
-    val users: LiveData<List<User>> = _users
+    private val _users = MutableLiveData<NetworkResponse<List<User>>>()
+    val users: LiveData<NetworkResponse<List<User>>> = _users
 
     fun fetchUsers(){
+
+        _users.value = NetworkResponse.Loading
         viewModelScope.launch {
-            val results = userRepository.fetchUsers()
-            _users.value = results
+            try {
+                val results = userRepository.fetchUsers()
+                results.let {
+                    _users.value = NetworkResponse.Success(it)
+                }
+            } catch (e: FirebaseFirestoreException){
+                _users.value = NetworkResponse.Error("failed to load data")
+            }
+
         }
     }
 
